b3f72a201509a69332dc49ca7fb08a71495fb144
Selvav096 <51014416+Selvav096@users.noreply.github.com>
2019-05-27 21:34:23 +0530

    Update Even.java

--- Even.java
+++ Even.java
@@ -3,7 +3,7 @@ public class Main
 {
 public static void main(String args[])
 {
-Scanner s=new Scanner (System.in)
+Scanner s=new Scanner (System.in);
 int a=s.nextInt();
 if(a%2==0)
 System.out.print("Even");
